bdb6681098cd8c7beb6a5fb0b509326f6a5f6ed6
isma sant <vigismael25@gmail.com>
2020-07-28 17:21:57 -0300

    Trabalhando com variaveis e alerts dentro do js.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "Isma sant ";
+var n1 = 5;
+var n2 = 3;
+var frase = "gremio é o melhor time do mundo";
+//alert(nome + "tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("gremio", "brasil"));
