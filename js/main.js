e13b7118553e955d3b8e68472a2019c6ec47bc6c
isma sant <vigismael25@gmail.com>
2020-08-03 11:30:54 -0300

    Trabalhando com Array e Dicionário.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 
-var nome = "Isma sant ";
-var n1 = 5;
-var n2 = 3;
-var frase = "gremio é o melhor time do mundo";
+
+
+
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"pera", cor:"amarelo"}]
+console.log(frutas);
+alert(frutas[0].nome);
+
+
+//var lista = ["maça", "pera", "jaca"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" + "));
+//var nome = "Isma sant ";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "gremio é o melhor time do mundo";
 //alert(nome + "tem " + idade + " anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toUpperCase());
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
 //alert(frase.replace("gremio", "brasil"));
