8e95aaabe33ce60f63862404b7dedbef36358c2f
isma sant <vigismael25@gmail.com>
2020-08-03 19:33:45 -0300

    Trabalhando com if & else, while e  for

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 
+var d = new Date();
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getSeconds());
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+/*
+var idade = prompt("Qual a sua idade?");
+if (idade >=18) {
+ alert("maior de idade");
+} else {
+alert("menor de idade"); 
+};
+*/
 
-
-
-
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"pera", cor:"amarelo"}]
-console.log(frutas);
-alert(frutas[0].nome);
-
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"pera", cor:"amarelo"}]
+//console.log(frutas);
+//alert(frutas[0].nome);
 
 //var lista = ["maça", "pera", "jaca"];
 //lista.push("uva");
 //lista.pop();
-
 //console.log(lista);
 //console.log(lista.toString());
 //console.log(lista.join(" + "));
